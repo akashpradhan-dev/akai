c64d3f7f60700c203e191c054767c20ae0b663dc
Akash Pradhan <akashpradhan151@gmail.com>
2025-05-21 00:28:27 +0530

    clark test

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ClerkProvider } from "@clerk/nextjs";
+const PUBLISHABLE_KEY =
+  process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY ?? "test_pk";
 
 interface ProvidersProps {
   children: React.ReactNode;
@@ -11,9 +13,7 @@ const queryClient = new QueryClient();
 export const Providers = ({ children }: ProvidersProps) => {
   return (
     <>
-      <ClerkProvider
-        publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
-      >
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
         <QueryClientProvider client={queryClient}>
           {children}
         </QueryClientProvider>
