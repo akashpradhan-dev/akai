f92e395e3f9d14c5e423e43601081afe31659044
Akash Pradhan <akashpradhan151@gmail.com>
2025-06-04 00:06:25 +0530

    fixed

--- src/components/StartButton.tsx
+++ src/components/StartButton.tsx
@@ -5,11 +5,10 @@ import React from "react";
 
 export const StartButton = () => {
   const { user } = useAuth();
-  console.log("user", user);
 
   return (
     <Link
-      href={user ? "/chart" : "/login"}
+      href={user ? "/chart/new" : "/login"}
       className="mt-5 px-6 py-3 bg-[#217bfe] text-white text-sm rounded-full transition hover:bg-white hover:text-[#217bfe] border border-transparent hover:border-[#217bfe]"
     >
       Get Started
